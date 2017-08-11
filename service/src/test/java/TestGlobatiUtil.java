9690c2673d881ee382ea9899d9cdfeddf0d9e1cd
Daniel <danielptm@me.com>
2017-08-11 09:20:34 -0700

    added change to test file

--- service/src/test/java/TestGlobatiUtil.java
+++ service/src/test/java/TestGlobatiUtil.java
@@ -11,6 +11,7 @@ import com.globati.utildb.HelpObjects.HashedPassword;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,7 @@ import java.util.List;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/spring/DealServiceTest-context.xml"})
 @ActiveProfiles("test")
+@Ignore
 public class TestGlobatiUtil {
 
     private static final Logger log = LogManager.getLogger(TestGlobatiUtil.class);
