df54f78e6a0ce352a35d219ac684ae5e3e4f3da6
Daniel <danielptm@me.com>
2017-09-01 21:05:50 +0200

    resolved conflict in employee

--- dbmodel/src/main/java/com/globati/dbmodel/Employee.java
+++ dbmodel/src/main/java/com/globati/dbmodel/Employee.java
@@ -25,11 +25,7 @@ public class Employee extends BaseEntity {
     private String image2;
     @Column(name="image3")
     private String image3;
-<<<<<<< HEAD
     @Column(length=300, name="email", nullable = true)
-=======
-    @Column(unique=true, length=300, name="email")
->>>>>>> employeeItemsObject
     private String email;
     @Column(unique = true, length=100, name="paypalemail")
     private String paypalEmail;
