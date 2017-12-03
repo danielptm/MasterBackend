4f3c185d2e7226e4ea202f951c94ac449c95b86b
Daniel <danielptm@me.com>
2017-12-03 20:46:21 +0100

    Added some abstract classes and  FlightBooking class in DbModel

---
+++ dbmodel/src/main/java/com/globati/dbmodel/BookingEntity.java
@@ -0,0 +1,7 @@
+package com.globati.dbmodel;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class BookingEntity extends BaseEntity {
+}
---
+++ dbmodel/src/main/java/com/globati/dbmodel/FlightBooking.java
@@ -0,0 +1,17 @@
+package com.globati.dbmodel;
+
+public class FlightBooking extends BookingEntity{
+
+    String time;
+    String paidstatus;
+    String cost;
+    String commission;
+    String plan;
+    String persons;
+    String departureDate;
+    String returndate;
+    String marker;
+    String company;
+
+    public FlightBooking() {}
+}
