7c8feb0c6756bf7ac7d9be309b9043d3636762e3
Daniel <danielptm@me.com>
2017-12-14 09:45:16 +0100

    changed scheduled task

--- service/src/main/java/com/globati/service/FlightBookingService.java
+++ service/src/main/java/com/globati/service/FlightBookingService.java
@@ -28,7 +28,7 @@ public class FlightBookingService {
     @Autowired
     FlightBookingRepository flightBookingRepository;
 
-    @Scheduled(cron = "0 43 9 * * ?")
+    @Scheduled(cron = "0 55 10 * * ?")
     public boolean getFlightBookingFroms3() throws Exception {
 
         try {
