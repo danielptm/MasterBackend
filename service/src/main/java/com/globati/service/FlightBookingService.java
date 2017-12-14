5e112565d379cabe4fbfc2819f79f32720006f6a
Daniel <danielptm@me.com>
2017-12-14 09:55:49 +0100

    changed time for scheudled task

--- service/src/main/java/com/globati/service/FlightBookingService.java
+++ service/src/main/java/com/globati/service/FlightBookingService.java
@@ -28,7 +28,7 @@ public class FlightBookingService {
     @Autowired
     FlightBookingRepository flightBookingRepository;
 
-    @Scheduled(cron = "0 55 10 * * ?")
+    @Scheduled(cron = "0 5 10 * * ?")
     public boolean getFlightBookingFroms3() throws Exception {
 
         try {
