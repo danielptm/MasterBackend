72d3d3a74078edcb8f6cfdadabc3707af7550941
Daniel <danielptm@me.com>
2017-12-14 10:30:55 +0100

    chnaged time on scheduled task

--- service/src/main/java/com/globati/service/FlightBookingService.java
+++ service/src/main/java/com/globati/service/FlightBookingService.java
@@ -28,7 +28,7 @@ public class FlightBookingService {
     @Autowired
     FlightBookingRepository flightBookingRepository;
 
-    @Scheduled(cron = "0 5 10 * * ?")
+    @Scheduled(cron = "0 45 10 * * ?")
     public boolean getFlightBookingFroms3() throws Exception {
 
         try {
