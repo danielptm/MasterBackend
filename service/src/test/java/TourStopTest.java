b4759f0178f6d48153947d841eeccba8f6dc284d
Daniel <danielptm@me.com>
2018-12-15 15:14:37 -0800

    Added Test

--- service/src/test/java/TourStopTest.java
+++ service/src/test/java/TourStopTest.java
@@ -1,5 +1,11 @@
+import com.globati.dbmodel.Property;
 import com.globati.dbmodel.TourStop;
+import com.globati.request.tour.Tour;
+import com.globati.service.PropertyService;
+import com.globati.service.TourService;
 import com.globati.service.TourStopService;
+import com.globati.service.exceptions.ServiceException;
+import com.globati.service.exceptions.UserNameIsNotUniqueException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +15,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/spring/DealServiceTest-context.xml"})
@@ -18,9 +25,79 @@ public class TourStopTest {
     @Autowired
     TourStopService tourStopService;
 
+    @Autowired
+    PropertyService propertyService;
+
+    @Autowired
+    TourService tourService;
+
+    public String getRandomString() {
+        return UUID.randomUUID().toString();
+    }
+
     @Test
-    public void testMappingTourStops(){
+    public void getTourStopsByTourId() throws UserNameIsNotUniqueException, ServiceException {
+        Property property = propertyService.createProperty(
+                "propertyName",
+                getRandomString() + "@me.com",
+                getRandomString() + "@me.com",
+                "password",
+                11.11,
+                11.11,
+                "image",
+                "street",
+                "city",
+                "country"
+        );
+        Tour tour = new Tour();
+
+        tour.setPropertyId(property.getId());
+        tour.setCity("city");
+        tour.setCountry("country");
+        tour.setTitle("title");
+        tour.setTargetLat(11.11);
+        tour.setTargetLong(11.11);
+
+        Tour tour2 = new Tour();
+
+        tour2.setPropertyId(property.getId());
+        tour2.setCity("city");
+        tour2.setCountry("country");
+        tour2.setTitle("title");
+        tour2.setTargetLat(11.11);
+        tour2.setTargetLong(11.11);
+
+        List<String> images = new ArrayList<>();
+        images.add("image1");
+        images.add("image2");
+
+        tour.setImages(images);
+        tour2.setImages(images);
+
+
         List<com.globati.request.tour.TourStop> tourStops = new ArrayList<>();
+
+        com.globati.request.tour.TourStop tourStop = new com.globati.request.tour.TourStop();
+
+        tourStop.setCity("tourStopCity");
+        tourStop.setCountry("tourStopCountry");
+        tourStop.setStreet("tourStreet");
+        tourStop.setDescription("tourStopDescription");
+        tourStop.setId(1L);
+        tourStop.setTargetLat(11.11);
+        tourStop.setTargetLong(11.11);
+        tourStop.setTitle("tourStopTitle");
+
+        tourStops.add(tourStop);
+
+
+        tour.setTourStops(tourStops);
+        tour.setImages(images);
+
+        com.globati.dbmodel.Tour createdTour = tourService.createTour(tour);
+
+//        List<TourStop> createdTourStops = tourStopService.mapRequestTourStopsToDbModelTourStops(createdTour, tourStops);
+
 
 
 
