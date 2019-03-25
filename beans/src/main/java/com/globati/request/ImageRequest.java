188039fb6f63f8be81a34281629e91b51190f113
Daniel <danielptm@me.com>
2019-03-24 20:12:24 -0700

    Added CRUD operations to DynamoImageService

---
+++ beans/src/main/java/com/globati/request/ImageRequest.java
@@ -0,0 +1,30 @@
+package com.globati.request;
+
+public class ImageRequest {
+    String id;
+    String path;
+
+    public ImageRequest() {
+    }
+
+    public ImageRequest(String id, String path) {
+        this.id = id;
+        this.path = path;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
--- service/src/main/java/com/globati/service/dynamodb/DynamoImageService.java
+++ service/src/main/java/com/globati/service/dynamodb/DynamoImageService.java
@@ -1,7 +1,26 @@
 package com.globati.service.dynamodb;
 
+import com.globati.dynamodb.DynamoProperty;
+import com.globati.dynamodb.common.DynamoImage;
+import com.globati.request.ImageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DynamoImageService {
+
+    public DynamoProperty createDynamoImage(ImageRequest imageRequest) {
+        return null;
+    }
+
+    public DynamoImage getDynamoImageById() {
+        return null;
+    }
+
+    public DynamoProperty updateDynamoImage () {
+        return null;
+    }
+
+    public DynamoProperty deleteDynamoImage () {
+        return null;
+    }
 }
