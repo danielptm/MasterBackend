7c0e4a4f8750a9fa5c077122c42eed6bc9b4ea49
Daniel <danielptm@me.com>
2019-04-17 21:17:50 -0700

    changes

--- server_start.sh
+++ server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server/api/target
-sudo /usr/bin/java -jar -Dserver.port=22 \
+sudo /usr/bin/java -jar -Dserver.port=80 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
