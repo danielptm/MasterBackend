4d8948a855595a5543216b1872cca7088db8b913
Daniel <danielptm@me.com>
2019-04-18 19:33:55 -0700

    cjanges

--- server_start.sh
+++ server_start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+export GLOBATI_SERVER_ENV="prod"
 mv /home/ec2-user/server/api/target/application.jar /home/ec2-user/server/application.jar
 java -jar application.jar -Dserver.port=80
