18fc990954cb3156f6b7b04fea1c0469daed2880
Daniel <danielptm@me.com>
2019-04-18 19:39:59 -0700

    changes

--- server_start.sh
+++ server_start.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-export GLOBATI_SERVER_ENV="prod"
 mv /home/ec2-user/server/api/target/application.jar /home/ec2-user/server/application.jar
 java -jar application.jar -Dserver.port=80
