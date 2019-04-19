72fb33b0a26d0a850b9beee7b24f9f618993de3b
Daniel <danielptm@me.com>
2019-04-18 20:51:23 -0700

    added changes

--- server_start.sh
+++ server_start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo export GLOBATI_SERVER_ENV="prod" > ~/.bash_profile
+echo export GLOBATI_SERVER_ENV="production" > ~/.bash_profile
 profile ~/.bash_profile
 mv /home/ec2-user/server/api/target/application.jar /home/ec2-user/server/application.jar
 java -jar application.jar -Dserver.port=80
