b81e7faa19766355bd299bb51705fe7e8646811a
Daniel <danielptm@me.com>
2019-04-18 19:22:14 -0700

    changes

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mv /home/ec2-user/server/api/target/application.jar /home/ec2-user/server/application.jar
-
 if java -version | grep -q "java version" ; then
   sudo yum install java-1.8.0 -y
 else
--- server_start.sh
+++ server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/server/api/target
+mv /home/ec2-user/server/api/target/application.jar /home/ec2-user/server/application.jar
 kill -9 PID
 java -jar application.jar -Dserver.port=80
