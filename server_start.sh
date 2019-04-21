f3dffc92f7ef3382c25d49d46bfb839816810be1
Daniel <danielptm@me.com>
2019-04-21 13:44:51 -0700

    changes

--- server_start.sh
+++ server_start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+
 export GLOBATI_SERVER_ENV="production"
 sudo source ~/.bash_profile
 mv /home/ec2-user/server/api/target/application.jar /home/ec2-user/server/application.jar
 cd /home/ec2-user/server
-sudo java -jar application.jar
+java -jar application.jar
